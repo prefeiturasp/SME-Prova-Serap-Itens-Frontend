19bb7af7a32754c462949987f566dac3fd5b9615
krisamcom <krishnamuti.barros@amcom.com.br>
2023-03-01 19:23:30 -0300

    ajustes
    - ajustes na estrutura de pastas de cadastro de itens
    - criar componente da tab ConfiguracaoItem
    - criar componente da tab ElaboracaoItem
    - criar componente de tabs do form

--- src/pages/item/cadastrar/index.tsx
+++ src/pages/item/cadastrar/index.tsx
@@ -1,18 +1,10 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Button, Col, Form, Row, Input, Space, notification, Spin } from 'antd';
-import AreaConhecimento from '~/components/configuracao-item/campos/area-conhecimento';
+import { Button, Col, Form, Row, Space, notification, Spin } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppState } from '~/redux';
-import Disciplina from '~/components/configuracao-item/campos/disciplina';
-import NivelEnsino from '~/components/configuracao-item/campos/nivel-ensino';
-import { DefaultOptionType } from 'antd/lib/select';
-import Matriz from '~/components/configuracao-item/campos/matriz';
-import ModeloMatriz from '~/components/configuracao-item/modelo-matriz';
 import { ItemDto } from '~/domain/dto/item-dto';
 import configuracaoItemService from '~/services/configuracaoItem-service';
 import './cadastroItemStyles.css';
-import { Tab } from 'rc-tabs/lib/interface';
-import { Tabs } from 'antd';
 import {
   setItem,
   setConfiguracaoItem,
@@ -25,9 +17,8 @@ import {
   ComponentesItemProps,
   ElaboracaoItemProps,
 } from '~/redux/modules/cadastro-item/item/reducers';
-import ComponentesItem from '~/components/configuracao-item/tabs/tab-componentes-item';
-import { Campos } from '~/domain/enums/campos-cadastro-item';
-import { Separador, Title } from '~/components/configuracao-item/campos/elementos';
+import { Title } from '~/components/cadastro-item/elementos';
+import TabForm from '~/components/cadastro-item/tab-form';
 
 const ItemCadastro: React.FC = () => {
   const dispatch = useDispatch();
@@ -36,17 +27,9 @@ const ItemCadastro: React.FC = () => {
   const configuracaoItem = useSelector((state: AppState) => state.configuracaoItem);
   const componentesItem = useSelector((state: AppState) => state.componentesItem);
   const elaboracaoItem = useSelector((state: AppState) => state.elaboracaoItem);
-  const matriz = useSelector((state: AppState) => state.matriz);
-  const disciplina = useSelector((state: AppState) => state.disciplina);
 
   const [objTabConfiguracaoItem, setObjTabConfiguracaoItem] =
     useState<ConfiguracaoItemProps>(configuracaoItem);
-  const [listaAreaConhecimento, setListaAreaConhecimento] = useState<DefaultOptionType[]>([]);
-  const [listaDisciplinas, setListaDisciplinas] = useState<DefaultOptionType[]>([]);
-  const [listaMatriz, setListaMatriz] = useState<DefaultOptionType[]>([]);
-
-  const [modelomatriz, setModeloMatriz] = useState<string>(matriz.modelo);
-  const [nivelEnsino, setNivelEnsino] = useState<string>(disciplina.nivelEnsino);
 
   const [form] = Form.useForm();
   const initialValuesForm = {
@@ -54,10 +37,6 @@ const ItemCadastro: React.FC = () => {
     infoEstatisticasDificuldade: '',
     infoEstatisticasAcertoCasual: '',
   };
-
-  const disciplinaidForm = Form.useWatch(Campos.disciplinas, form);
-  const areaConhecimentoIdForm = Form.useWatch(Campos.areaConhecimento, form);
-  const matrizIdForm = Form.useWatch(Campos.matriz, form);
 
   const [itemSalvar, setItemSalvar] = useState<ItemDto>({} as ItemDto);
 
@@ -221,90 +200,6 @@ const ItemCadastro: React.FC = () => {
     dadosItemSalvar,
   ]);
 
-  useEffect(() => {
-    form.resetFields();
-  }, [form, listaAreaConhecimento]);
-
-  useEffect(() => {
-    const novoObj: ConfiguracaoItemProps = {
-      codigo: configuracaoItem.codigo,
-      areaConhecimento: areaConhecimentoIdForm,
-      disciplina: disciplinaidForm,
-      matriz: matrizIdForm,
-    };
-    setObjTabConfiguracaoItem(novoObj);
-  }, [disciplinaidForm, areaConhecimentoIdForm, matrizIdForm, configuracaoItem]);
-
-  useEffect(() => {
-    dispatch(setConfiguracaoItem(objTabConfiguracaoItem));
-  }, [objTabConfiguracaoItem, dispatch]);
-
-  const contentTabConfiguracao: React.ReactNode = (
-    <>
-      <div id='tabConfiguracao'>
-        <Row gutter={2}>
-          <Col span={8}>
-            <Form.Item label='Código'>
-              <Input
-                disabled={true}
-                placeholder='Código Item'
-                value={configuracaoItem?.codigo > 0 ? configuracaoItem.codigo : ''}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={10}>
-          <Col span={8}>
-            <AreaConhecimento
-              form={form}
-              options={listaAreaConhecimento}
-              setArea={setListaAreaConhecimento}
-            ></AreaConhecimento>
-          </Col>
-          <Col span={8}>
-            <Disciplina
-              form={form}
-              options={listaDisciplinas}
-              setDisciplinas={setListaDisciplinas}
-            ></Disciplina>
-          </Col>
-          <Col span={8}>
-            <Matriz form={form} options={listaMatriz} setMatrizes={setListaMatriz}></Matriz>
-          </Col>
-        </Row>
-        <Separador />
-        <Row gutter={10}>
-          <Col>
-            <ModeloMatriz
-              setModeloMatriz={setModeloMatriz}
-              modelo={modelomatriz}
-              form={form}
-            ></ModeloMatriz>
-          </Col>
-        </Row>
-        <Row gutter={10}>
-          <Col>
-            <NivelEnsino
-              setNivelEnsino={setNivelEnsino}
-              nivelEnsino={nivelEnsino}
-              form={form}
-            ></NivelEnsino>
-          </Col>
-        </Row>
-      </div>
-    </>
-  );
-
-  const tabs: Array<Tab> = [
-    { key: '1', label: 'Configuração', children: contentTabConfiguracao },
-    {
-      key: '2',
-      label: 'Componentes do item',
-      children: <ComponentesItem form={form} />,
-    },
-    { key: '3', label: 'Elaboração do item', children: <h1>Elaboração do item</h1> },
-  ];
-
   return (
     <>
       <Spin size='small' spinning={carregando}>
@@ -335,7 +230,7 @@ const ItemCadastro: React.FC = () => {
           autoComplete='off'
           initialValues={initialValuesForm}
         >
-          <Tabs className='margemTabs' defaultActiveKey='1' type='card' items={tabs} />
+          <TabForm form={form} />
         </Form>
       </Spin>
     </>
