ea27054f78f788f133c24f3b7d140228c7658f12
Anderson Oliveira Magalhães <andoliveira4@gmail.com>
2025-10-21 14:01:00 -0300

    primeira versão

--- src/pages/listagemItens/listagemItens.tsx
+++ src/pages/listagemItens/listagemItens.tsx
@@ -1,4 +1,4 @@
-import { Col, Row } from 'antd';
+import { Card, Col, Row } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowLeftOutlined } from '@ant-design/icons';
@@ -7,24 +7,64 @@ import './listagemItens.css';
 const ListagemItens: React.FC = () => {
   const linkRetorno = 'https://serap.sme.prefeitura.sp.gov.br/';
   return (
-    <div className='cadastrarItemHeader'>
-      <Row className='cadastrarItemHeader-corpo'>
-        <Col xs={12} md={6}>
-          <Link to={linkRetorno} className='cadastrarItemHeader-retornar'>
-            <ArrowLeftOutlined className='cadastrarItemHeader-icone-retornar' />
-            <span className='cadastrarItemHeader-texto-retornar'>Retornar à tela inicial</span>
-          </Link>
-        </Col>
-        <Col xs={12} md={12} className='cadastrarItemHeader-titulo'>
-          Cadastrar novo item
-        </Col>
-        <Col xs={0} md={6} />
-      </Row>
-      <div className='cadastrarItemHeader-rota'>
-        <div className='cadastrarItemHeader-rota-texto'>Home / Itens/ Cadastrar novo item</div>
-        <div className='cadastrarItemHeader-rota-titulo'>Cadastrar novo item</div>
+    <>
+      <div className='listagem-pagina'>
+        <div className='cadastrarItemHeader'>
+          <Row className='cadastrarItemHeader-corpo'>
+            <Col xs={12} md={6}>
+              <Link to={linkRetorno} className='cadastrarItemHeader-retornar'>
+                <ArrowLeftOutlined className='cadastrarItemHeader-icone-retornar' />
+                <span className='cadastrarItemHeader-texto-retornar'>Retornar à tela inicial</span>
+              </Link>
+            </Col>
+            <Col xs={12} md={12} className='cadastrarItemHeader-titulo'>
+              Cadastrar novo item
+            </Col>
+            <Col xs={0} md={6} />
+          </Row>
+          <div className='cadastrarItemHeader-rota'>
+            <div className='cadastrarItemHeader-rota-texto'>Home / Itens/ Cadastrar novo item</div>
+            <div className='cadastrarItemHeader-rota-titulo'>Cadastrar novo item</div>
+          </div>
+        </div>
+
+        <div className='uuu'>
+          <Row className='listagem-titulo'>
+            <Col xs={12} md={12}>
+              Lista de itens
+            </Col>
+          </Row>
+
+          <Row className='listagem-subtitulo'>
+            <Col xs={12} md={12}>
+              Sua lista de itens criados. você pode conferir detalhes, fazer edições ou usar os
+              filtros para encontrar o que precisa.
+            </Col>
+          </Row>
+        </div>
+
+        <Card className='listagem-busca'>
+          <div>BUSCA VEM AQUI</div>
+        </Card>
+
+        <div className='listagem-conteudo'>
+          <Card className='listagem-tabela'>
+            <div>
+              <div>
+                <div>Lista de itens</div>
+                <div>FILTRAR</div>
+                <div>Selecione um item para conferir mais detalhes ao lado.</div>
+              </div>
+              <div>TABELA</div>
+            </div>
+          </Card>
+
+          <Card className='listagem-tabela'>
+            <div>Resumo Do Item</div>
+          </Card>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
