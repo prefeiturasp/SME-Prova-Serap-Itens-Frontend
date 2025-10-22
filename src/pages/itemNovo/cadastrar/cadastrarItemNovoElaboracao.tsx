772e2d9e3ab6f8df5e78c8a61a4a4833b8068af8
Lincoln <link7-7@hotmail.com>
2025-10-22 13:12:52 -0300

    estrutura inicial

--- src/pages/itemNovo/cadastrar/cadastrarItemNovoElaboracao.tsx
+++ src/pages/itemNovo/cadastrar/cadastrarItemNovoElaboracao.tsx
@@ -1,5 +1,5 @@
-import { Col, Form, FormProps, notification, Row, Spin } from "antd";
 import React, { useCallback, useState } from "react";
+import { Button, Form, FormProps, notification, Spin } from 'antd';
 
 //css
 import './cadastrarItemNovo.css';
@@ -40,10 +40,55 @@ const CadastrarItemNovoElaboracao: React.FC<FormProps> = ({form}) => {
                 >
                     {/* <Affix offsetTop={0.1} style={{ marginBottom: 30 }}> */}
                     <CadastrarItemHeaderComponent pagina={2} />
-                    
-
                     {/* </Affix> */}
 
+                     <div className='cadastrarItem-corpo'>
+                        <div className='cadastrarItem-titulo-corpo'>
+                            <div className='cadastrarItem-titulo'>
+                                Configure o novo item
+                            </div>
+                            <div className='cadastrarItem-subtitulo'>
+                                Preencha as informações abaixo para criar e cadastrar um novo item. Esses dados garantem que ele esteja alinhado à matriz de avaliação e possa ser aplicado corretamente.
+                            </div>
+                        </div>
+
+
+
+                        <div className='cadastrarItem-botoes'>
+                            <div className='cadastrarItem-btn'>
+                                <Button className='btnVoltar' 
+                                // onClick={voltar}
+                                >
+                                    Voltar
+                                </Button>
+                            </div>
+                            <div className='cadastrarItem-btn'>
+                                <Button
+                                    type='primary'
+                                    // onClick={() => salvarItem(true)}
+                                    // disabled={bloquearBtnSalvarRascunho}
+                                    className='btnRascunho'
+                                >
+                                    Salvar rascunho
+                                </Button>
+                            </div>
+                            <div className='cadastrarItem-btn'>
+                                <Button className='btnAvancar' 
+                                // onClick={voltar}
+                                >Avançar</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='cadastrarItem-footer'>
+                        <div className='cadastrarItem-footer-conteudo'>
+                            <div className='footer-item1'>
+                                SERAp - Versão: 1.30.9.2
+                            </div>
+                            <div className='footer-item2'>
+                                Todos os direitos reservados
+                            </div>
+                        </div>
+                    </div>
                 </Form>
             </Spin>
         </>
