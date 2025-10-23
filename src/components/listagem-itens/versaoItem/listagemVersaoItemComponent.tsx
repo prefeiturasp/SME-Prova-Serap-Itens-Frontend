9d1012e4d38438c4292e92f802f9d8ed2d3b0754
Anderson Oliveira Magalhães <andoliveira4@gmail.com>
2025-10-23 08:43:24 -0300

    criacao do novo componente e ajuste gerais

---
+++ src/components/listagem-itens/versaoItem/listagemVersaoItemComponent.tsx
@@ -0,0 +1,9 @@
+import { Card } from 'antd';
+import React from 'react';
+import './listagemVersaoItemComponent.css';
+
+const ListagemVersaoItemComponent: React.FC = () => {
+  return <Card>***VERSAO VEM AQUI***</Card>;
+};
+
+export default ListagemVersaoItemComponent;
