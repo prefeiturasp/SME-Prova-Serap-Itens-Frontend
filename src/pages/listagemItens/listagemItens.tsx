ae1504acdc40b9944e404a1386e2b70beedc07b8
Mario Junior <mariosj9@gmail.com>
2025-10-24 16:31:57 -0300

    AB#136366

--- src/pages/listagemItens/listagemItens.tsx
+++ src/pages/listagemItens/listagemItens.tsx
@@ -8,6 +8,7 @@ import ListagemSelectComponent from '~/components/listagem-itens/select/listagem
 import ListagemResumoItemComponent from '~/components/listagem-itens/resumoItem/listagemResumoItemComponent';
 import ListagemVersaoItemComponent from '~/components/listagem-itens/versaoItem/listagemVersaoItemComponent';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import type { VersaoDto } from '~/domain/dto/versao-dto';
 
 const { Text } = Typography; // ✅ corrigido: define o Text do Ant Design
 interface Item {
@@ -95,6 +96,11 @@ const ListagemItens: React.FC = () => {
   const fim = inicio + ITENS_POR_PAGINA;
   const itensPagina = tabelaItens.slice(inicio, fim);
 
+  const versoes: VersaoDto[] = [
+    { id: 1, codigoItem: '_LPT_EF4_SAEB_00', versaoItem: 1, dataCriacao: '05/10/2025', provas: [] },
+    { id: 2, codigoItem: '_LPT_EF4_SAEB_00', versaoItem: 2, dataCriacao: '30/11/2020', provas: [] },
+  ];
+
   return (
     <div className='listagem-pagina'>
       <div className='cadastrarItemHeader'>
@@ -167,7 +173,7 @@ const ListagemItens: React.FC = () => {
             <ListagemResumoItemComponent></ListagemResumoItemComponent>
           </div>
           <div>
-            <ListagemVersaoItemComponent></ListagemVersaoItemComponent>
+            <ListagemVersaoItemComponent versoes={versoes}></ListagemVersaoItemComponent>
           </div>
         </Card>
       </div>
