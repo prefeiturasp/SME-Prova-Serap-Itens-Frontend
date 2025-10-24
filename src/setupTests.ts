a215317199945fe1ef6c84f0cd92002333ecba38
Anderson Oliveira Magalhães <andoliveira4@gmail.com>
2025-10-24 15:34:29 -0300

    adicionando testes unitarios componente de listagem

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: any) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {}, // compatibilidade antiga
+    removeListener: () => {}, // compatibilidade antiga
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
