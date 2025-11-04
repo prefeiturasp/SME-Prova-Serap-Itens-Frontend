88ebc52664cb7bea67f2a6462c793959a67e1eff
Mario Junior <mariosj9@gmail.com>
2025-11-04 16:28:21 -0300

    AB#136394

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom/client';
+import './main.css';
 import App from './app';
-
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-    <App />
+  <App />,
   // </React.StrictMode>,
 );
--- src/pages/403/index.tsx
+++ src/pages/403/index.tsx
@@ -1,19 +1,64 @@
-import { Button, Result } from 'antd';
+import { Button, Card } from 'antd';
 import { voltarAoSerap } from '~/utils/converte-dto';
 
 const NaoAutenticado = () => {
   return (
-    <Result
-      status='403'
-      title='Desculpe, você não está autorizado a acessar esta página'
-      subTitle='Volte ao SERAp e tente novamente'
-      extra={
-        <Button type='primary' onClick={() => voltarAoSerap()}>
-          Voltar
+    <div style={styles.container}>
+      <Card bordered={false} style={styles.card}>
+        <div style={styles.imageContainer}>
+          <img src='/acesso_negado.svg' alt='Acesso Negado' style={styles.image} />
+        </div>
+        <h2 style={styles.title}>Desculpe, você não está autorizado a acessar esta página</h2>
+        <p style={styles.subtitle}>
+          Para acessar, primeiro você precisa realizar o seu login com usuário e senha.
+        </p>
+
+        <Button type='primary' size='large' style={styles.button} onClick={voltarAoSerap}>
+          Fazer login
         </Button>
-      }
-    />
+      </Card>
+    </div>
   );
+};
+
+const styles: { [key: string]: React.CSSProperties } = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    textAlign: 'center',
+  },
+  imageContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: '20px',
+  },
+  image: {
+    width: '300px',
+    height: 'auto',
+  },
+  card: {
+    width: '100%',
+    maxWidth: '500px',
+    padding: '20px',
+    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+  },
+  title: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: '14px',
+    color: '#666',
+    marginBottom: '20px',
+  },
+  button: {
+    width: '150px',
+  },
 };
 
 export default NaoAutenticado;
